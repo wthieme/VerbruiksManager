30ea4edd45078bb25c90e84408e76b4cca4dbeab
Wilhelm <wilhelm.thieme@gmail.com>
2017-11-07 22:39:34 +0100

    Latest Android Libraries Grafiek netter gemaakt

--- app/src/main/java/nl/whitedove/verbruiksmanager/StatsComparator.java
+++ app/src/main/java/nl/whitedove/verbruiksmanager/StatsComparator.java
@@ -3,7 +3,7 @@ package nl.whitedove.verbruiksmanager;
 import java.util.Comparator;
 
 public class StatsComparator implements Comparator<VerbruikStat> {
-    public static final StatsComparator instance=new StatsComparator();
+    static final StatsComparator instance=new StatsComparator();
 
     public int compare(VerbruikStat left, VerbruikStat right) {
         return Double.compare(right.getVerbruik(), left.getVerbruik());
